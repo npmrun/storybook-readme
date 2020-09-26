9d66654b18a55ad74bad728c21770749caea7d8f
npmrun <kk1549469775@gmail.com>
2020-09-26 17:06:14 +0800

    delete

--- packages/example-react/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testURL: 'http://localhost',
-  transform: {
-    '^.+\\.md?$': 'markdown-loader-jest',
-    '^.+\\.js?$': 'babel-jest',
-  },
-  transformIgnorePatterns: ['/node_modules/(?!(@babel/runtime)/).*/'],
-};
--- packages/example-react/stories/withPropsTable/index.js
+++
@@ -1,6 +0,0 @@
-import './basic';
-import './multipleComponents';
-import './hocPattern';
-import './renderPropPattern';
-import './excludeGlobally';
-import './includeLocally';
